bd3b8c4ddceebc71b2e6a68a520d06cadf552cd6
Sruthi Bellamkonda <sruthibellamkonda2928@gmail.com>
2024-11-13 13:36:45 -0400

    Add Styles

--- gdox-client/src/TextEditor.js
+++ gdox-client/src/TextEditor.js
@@ -2,14 +2,30 @@ import React, { useCallback, useEffect, useRef } from 'react'
 import Quill from 'quill'
 import "quill/dist/quill.snow.css"
 
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+  ]
+
+  
 export default function TextEditor() {
     const wrapperRef = useCallback((wrapper) => {
         if(wrapper == null) return
         wrapper.innerHTML = ''
         const editor = document.createElement('div')
         wrapper.append(editor)
-        new Quill(editor, { theme: 'snow' })
-        
+        new Quill(editor, { 
+            theme: 'snow',
+            modules: {toolbar: TOOLBAR_OPTIONS}
+         })
+
     }, [])
   return (
     <div id="container" ref={wrapperRef}></div>
