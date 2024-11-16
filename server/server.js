b48b7967260bd87180c05767d0579a8b72393c0f
Sruthi Bellamkonda <sruthibellamkonda2928@gmail.com>
2024-11-16 14:25:26 -0400

    Handle Multiple docs

--- server/server.js
+++ server/server.js
@@ -1,12 +1,38 @@
-const io = require('socket.io')(3001, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+const mongoose = require("mongoose")
+const Document = require("./Document")
+
+mongoose.connect("mongodb://127.0.0.1:27017/", {
 })
 
-io.on("connection", socket => {
-    socket.on('send-changes', delta => {
-        socket.broadcast.emit('receive-changes', delta)
-    })
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 })
+
+const defaultValue = ""
+
+io.on("connection", socket => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+})
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
